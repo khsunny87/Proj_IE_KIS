2e4ea082671440a949a141956e6938137a3e06b3
Sun Kim <yoursunny@gmail.com>
2021-06-12 13:17:43 +0900

    RMD_result 생성 변수 오류 수정

--- R_script/02_Rendering_and_Inclusion.R
+++ R_script/02_Rendering_and_Inclusion.R
@@ -1,4 +1,5 @@
 library(tidyr)
+library(lubridate)
 
 num_logical<-function(vec_x){
   
@@ -15,7 +16,12 @@ grade<-c('no','minimal','minimal-to-mild','mild','mild-to-moderate','moderate','
 inc_data<-raw_data%>%
   filter(Info_Inclusion_list=='TRUE')%>%
   mutate_at(vars(starts_with('Symptom_')),num_logical)%>%
+    mutate(Symptom_dyspnea=(Symptom_dyspnea|Symptom_orthopnea))%>%
+    select(-Symptom_orthopnea)%>%
+    mutate(Symptom_myalgia=(Symptom_myalgia|Symptom_abdominal_pain))%>%
+    select(-Symptom_abdominal_pain)%>%
   mutate_at(vars(starts_with('PMH_')),num_logical)%>%
+    select(-PMH_dyslipidemia,-PMH_COPD,-`PMH_비고`)%>%
   mutate_at(vars(starts_with('Mitral_')),num_logical)%>%
   mutate_at(vars(starts_with('Concomitant_')),num_logical)%>%
   mutate(Info_Sex=factor(`Info_성별`,levels=c('M','F')))%>%
@@ -27,11 +33,8 @@ inc_data<-raw_data%>%
     mutate(Aortic_AR=factor(Aortic_AR,levels=grade))%>%
     mutate(Aortic_AS=factor(Aortic_AS,levels=grade))%>%
     mutate(Tricuspid_TR=factor(Tricuspid_TR,levels=grade))%>%
-    mutate(Tricuspid_TS=factor(Tricuspid_TS,levels=grade))
+    mutate(Tricuspid_TS=factor(Tricuspid_TS,levels=grade))%>%
+  mutate(Duration_Onset2Op=(`Info_수술일`-`Acute_Sx._onset`)/ddays(1),Duration_Anti2Op=(`Info_수술일`-`Anti_투여날짜`)/ddays(1))
+
+
   
-
-  
-
-
-
-
--- R_script/04_Make_Tables.R
+++ R_script/04_Make_Tables.R
@@ -1,15 +1,32 @@
 library(moonBook)
 library(forcats)
+library(ztable)
 
-anal_data%>%
+
+myt_output<-'viewer' #default
+
+print_mytable<-function(myt){
+  
+  if(myt_output=='viewer'){
+    options(ztable.type='viewer')
+    myt%>%ztable()%>%print()
+    
+  } else if(myt_output=='HTML'){
+    options(ztable.type='HTML')
+    myt%>%ztable()%>%print()
+  } else if(myt_output=='PPT'){
+    myt%>%mytable2df()%>%knitr::kable(format='pipe',row.names=F)%>%print()
+  }
+  
+}
+
+
+table1<-anal_data%>%
   mutate(Mitral_MVR=fct_relevel(if_else(Mitral_MVR,'Replacement','Repair'),'Repair'))%>%
-  select(Mitral_MVR,Info_Sex,Info_Age,starts_with('Baseline_'),starts_with('Symptom_'),starts_with('PMH_'),starts_with('Lab_'),starts_with('CPB_'),starts_with('Concomitant_'),Mitral_MS,Mitral_MR)%>%
+  select(Mitral_MVR,Info_Sex,Info_Age,starts_with('Baseline_'),starts_with('Symptom_'),starts_with('PMH_'),starts_with('Lab_'),starts_with('Duration_'),starts_with('CPB_'),starts_with('Concomitant_'),Mitral_MS,Mitral_MR)%>%
   select(-CPB_TCA_time)%>%
   mytable(Mitral_MVR~.,data=.,show.total=T,catMethod=0)%>%
   compress(add.label=F)
 
   
 
-inc_data%>%
-  mytable(Mitral_MVR~PMH_HTN,data=.,show.total=T,catMethod=0)%>%
-  compress(add.label=F)
