650797121cf7746ed1f863c20df2570bdcc9e89d
Sun Kim <yoursunny@gmail.com>
2021-06-25 12:23:14 +0900

    table에 아웃컴 추가 EM이 IHM 포함하도록

--- R_script/04_Make_Tables.R
+++ R_script/04_Make_Tables.R
@@ -20,13 +20,15 @@ print_mytable<-function(myt){
   
 }
 
+
 baseline_tbl<-tbl_data%>%
   select(Mitral_Repair,Info_Male,Info_Age,starts_with('Baseline_'),starts_with('Symptom_'),IE_embolism,starts_with('PMH_'),
          IE_culture,starts_with('Lab_'),starts_with('Op_Ix_'),starts_with('Duration_'),
          Mitral_prev_MV_Op,Mitral_MS,Mitral_MR,MR_gr_mod,Mitral_annular_recon,
          Aortic_Prev_AV_Op,Aortic_IE_lesion,Aortic_AS,Aortic_AR,AR_gr_mod,Aortic_Concomittant_AV_Op,
          Tricuspid_Prev_TV_Op,Tricuspid_IE_lesion,Tricuspid_TS,Tricuspid_TR,TR_gr_mod,Tricuspid_Concomittant_TV_Op,
-         starts_with('Concomitant_'),starts_with('CPB_'))%>%
+         starts_with('Concomitant_'),starts_with('CPB_'),
+         O_Survival_Death,O_Survival_EM,O_Survival_IHM,O_Recurrence_Recur,O_Recurrence_ReOp,O_Valve_ReOp)%>%
   select(-CPB_TCA_time)%>%
   mutate(Mitral_Repair=fct_relevel(if_else(Mitral_Repair==1,'Repair','Replacement'),'Repair'))%>%
   mytable(Mitral_Repair~.,data=.,show.total=T,catMethod=0)%>%
